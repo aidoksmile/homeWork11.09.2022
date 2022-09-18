a61dd7c1135bbea4102b260c51d26a44c605f3dc
Aida <shakharova.aida@yandex.ru>
2022-09-18 12:53:58 +0600

    Найти сумму цифр числа

---
+++ 3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Найти сумму цифр числа
+
+int DigitsSum(int n)
+{
+    if (n == 0) return 0;
+    return n % 10 + DigitsSum(n / 10);
+}
+
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine($"Сумма цифр в числе {n} = {DigitsSum(n)}");
