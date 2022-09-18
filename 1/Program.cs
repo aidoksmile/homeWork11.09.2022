39184f63985a4ba65e537cd795f6023db5def085
Aida <shakharova.aida@yandex.ru>
2022-09-18 12:53:30 +0600

    Показать натуральные числа от M до N, N и M заданы

---
+++ 1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Показать натуральные числа от M до N, N и M заданы
+
+void PrintNumber(int m, int n)
+{
+    string a = "error";
+    if (n < m) return;
+        PrintNumber(m, n - 1);
+        Console.Write(n + " ");
+}
+
+Console.WriteLine("Введите M: ");
+int M = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите N: ");
+int N = int.Parse(Console.ReadLine());
+PrintNumber(M, N);
