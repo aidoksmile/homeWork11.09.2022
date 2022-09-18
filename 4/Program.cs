842bcd4689926c3f3cf4573579c0a5f5333b1a13
Aida <shakharova.aida@yandex.ru>
2022-09-18 12:54:12 +0600

    Написать программу вычисления функции Аккермана

---
+++ 4/Program.cs
@@ -0,0 +1,20 @@
+﻿// Написать программу вычисления функции Аккермана
+
+int Akkerman(int i, int j)
+{
+    if (i == 0)
+        return j + 1;
+    else
+        if ((i > 0) && (j == 0))
+            return Akkerman(i - 1, 1);
+        else
+        {
+            return Akkerman(i - 1, Akkerman(i, j - 1));
+        }
+}
+
+Console.WriteLine("Введите число i: ");
+int i = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число j: ");
+int j = int.Parse(Console.ReadLine());
+Console.WriteLine($"Функция Аккермана = {Akkerman(i, j)}");
