c4d284712e7ff8c2db68c3637ce3d8e04584ab5b
Aida <shakharova.aida@yandex.ru>
2022-09-18 12:54:31 +0600

    Написать программу возведения числа А в целую стень B

---
+++ 5/Program.cs
@@ -0,0 +1,16 @@
+﻿// Написать программу возведения числа А в целую стень B
+
+int ExpFunction(int a, int b) // Возведение в степень
+{
+    if (b == 0)
+        return 1;
+    else
+        return a * ExpFunction(a, b - 1);
+
+}
+
+Console.WriteLine("Введите число A: ");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите степень B: ");
+int B = int.Parse(Console.ReadLine());
+Console.WriteLine($"{A}^{B} = {ExpFunction(A, B)}");
